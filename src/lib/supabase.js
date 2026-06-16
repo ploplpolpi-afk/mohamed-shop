116192b2cff7c569c5d282a8f1c40703259b42be
mohamed mahmoud <ploplpolpi@gmail.com>
2026-06-16 17:08:23 +0300

    Fix supabase initialization

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,11 +1,6 @@
 // src/lib/supabase.js
-const SUPABASE_URL = "https://qsmugonirpveactzseo.supabase.co"; // تأكد من الرابط
-const SUPABASE_ANON_KEY = "sb_publishable_TylcZpVwFYGK3VdFl3cE9g_Te8HaguN"; 
+const SUPABASE_URL = "https://qsmugonirpveactzseo.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFzbXVnb25pcm5wdmVhY3R6c2VvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE0NDU1MDUsImV4cCI6MjA5NzAyMTUwNX0.J5-dkl1_dyHnYyoC-NcFcJSMfVFgMREHhayj4Xic4OE";
 
-// التحقق من وجود المكتبة قبل البدء
-if (window.supabase) {
-    window.supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-    console.log("✅ Supabase initialized successfully");
-} else {
-    console.error("❌ Supabase library not found!");
-}
+window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+console.log("Supabase Client initialized!");
